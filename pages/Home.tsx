7f23957e4f6f28384bb1befd567f3770fb2a976d
abderrahim <103074554+uodbafhm@users.noreply.github.com>
2026-01-30 15:09:37 +0100

    Add image section to Home component
    Added a new image section to the Home component.

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -162,7 +162,12 @@ const Home: React.FC = () => {
                   ))}
                 </div>
               </div>
-              <div className="hidden lg:block relative">
+              <div className="relative w-[380px] h-[380px] mx-auto rounded-full overflow-hidden bg-white shadow-2xl ring-4 ring-teal-100">
+                  <img 
+                  src="https://images.pexels.com/photos/3845806/pexels-photo-3845806.jpeg?auto=compress&cs=tinysrgb&w=1260" 
+                  alt="Dr Mapso Clinic" 
+                  className="w-full h-full object-contain"
+                  />
               </div>
             </div>
           </div>
